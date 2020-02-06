3c0ee1e13611342b5b25de97dd3bcb51933d5ca5
Raf Sobol <rafal.sobol@academytrainee.com>
2020-02-06 14:56:14 +0000

    Fixed it up again!

--- src/com/qa/dog/Runner.java
+++ src/com/qa/dog/Runner.java
@@ -4,11 +4,15 @@ public class Runner {
 	public static void main(String[] args) {
 		DogBuilder dogBuild = new DogBuilder();
 		Dog dog = dogBuild.buildAge(5).buildHouse(true).buildPrivates("boi").buildShade("dark red slash yellow").build();
-//		dogBuild.buildHouse(true);
-//		dogBuild.buildPrivates("boi");
-//		dogBuild.buildShade("dark red slash yellow");
-//		
-//		Dog dog = dogBuild.build();
+		
+		DogBuilder dogSlowlyBuilt = new DogBuilder();
+		dogSlowlyBuilt = dogSlowlyBuilt.buildAge(10);
+		dogSlowlyBuilt.buildHouse(true);
+		Dog dogSlow = dogSlowlyBuilt.build();
+		System.out.println(dogSlow.isDomesticated());
+
+
+		
 		System.out.println(dog.getAge());
 	}
 }
